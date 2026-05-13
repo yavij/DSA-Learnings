b0b0b37d3a073c8e841dccffbb5d3882b556269c
Vijay A <96858002+yavij@users.noreply.github.com>
2026-05-13 08:06:05 +0530

    Add: islands-and-treasure - submission-1

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-1.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void islandsAndTreasure(int[][] grid) {
+        int row = grid.length, column = grid[0].length;
+       
+        Queue<int[]> queue = new LinkedList<>();
+        //1. Add all the water position into the queue
+        for(int i=0;i<row;i++){
+            for(int j=0;j<column;j++){
+                if(grid[i][j]==0)
+                queue.add(new int[]{i, j});
+            }
+        }
+       
+       //2. Define direction
+       int[][] directions = {{-1,0}, {1,0}, {0,1}, {0,-1}};
+
+       while(!queue.isEmpty()){
+            int[] val = queue.poll();
+            int r = val[0];
+            int c = val[1];
+
+            for(int[] dir: directions){
+                int newR = r+dir[0];
+                int newC = c+dir[1];
+
+                //Boundary check
+                if(newR<0 || newC<0 || newR>=row || newC>=column)
+                continue;
+
+                if(grid[newR][newC] == Integer.MAX_VALUE){
+                    grid[newR][newC] = grid[r][c] + 1;
+                    queue.add(new int[]{newR, newC});
+                }
+            }
+
+       }
+        
+    }
+}
